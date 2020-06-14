d9b48ba80327123a1bb7c93938648e913a92a9a8
Satori32 <Huga8989+githubf@gmail.com>
2020-06-14 21:39:09 +0900

    プロジェクト ファイルを追加します。

---
+++ ABCプロブレム？？/ソース.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <numeric>
+
+typedef std::vector<std::uintmax_t> DType;
+
+DType PrimeFacterlizer(std::uintmax_t N) {
+
+	DType R;
+	for (std::uintmax_t i = 2; i <= N; i++) {
+		if (N % i == 0) {
+			R.push_back(i);
+			N /= i;
+			i--;
+		}
+	}
+
+	return R;
+}
+
+std::uintmax_t Radical(std::uintmax_t N) {
+	auto R = PrimeFacterlizer(N);
+
+	std::sort(R.begin(), R.end());
+
+	R.erase(std::unique(R.begin(), R.end()), R.end());
+
+	std::uintmax_t V = 1;
+	for (auto& o : R) {
+		V *= o;
+	}
+	
+	return N;
+
+}
+
+bool CrashABCProblem() {//not debug complete
+
+	for (std::uintmax_t A = 1; A <= std::numeric_limits<std::uintmax_t>::max(); A++) {
+		for (std::uintmax_t B = 1; B < A; B++) {
+			if (std::gcd(A - B, B) != 1) { continue; }
+			std::uintmax_t C = (A - B) + B;
+			auto R = Radical((A - B) * B * C);
+			double X = std::pow(R, 1 + std::numeric_limits<double>::epsilon());
+			if (X < C) {
+				std::cout << "A:" << A << ",B:" << B << ",C:" << C << ",Rad:" << R << std::endl;
+				break;
+			}
+		}
+	}
+	return true;
+}
+
+int main() {
+	std::uintmax_t N = 2 * 2 * 3 * 3 * 5 * 7;
+	auto R = PrimeFacterlizer(N);
+
+	for (auto& o : R) {
+		std::cout << o << ',';
+	}
+	std::cout << std::endl;
+
+	std::cout<<Radical(N) << std::endl;
+
+	CrashABCProblem();
+	return 0;
+}
